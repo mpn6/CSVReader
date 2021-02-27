c679be83689c2843ac808807b8bcad649e56b7f1
Michelle Nunez <mpnunez5@gmail.com>
2021-02-26 23:44:13 -0500

    Tested that the parser could create and read a Grades object

--- tests/CSVread.test.js
+++ tests/CSVread.test.js
@@ -1,8 +1,16 @@
 const CSVRead = require('../src/Read');
 const City = require('../src/Models/City')
+const Grade = require('../src/Models/Grade')
 const expect = require('expect');
+
 test('Can Read CSV File', () => {
     let filename  = 'data/worldcities.csv';
     let records = CSVRead.getRecords(filename, City);
     expect(records.length).toBe(6);
+});
+
+test('Can Read CSV File', () => {
+    let filename  = 'data/grades.csv';
+    let records = CSVRead.getRecords(filename, Grade);
+    expect(records.length).toBe(4);
 });
--- tests/cities.test.js
+++ tests/cities.test.js
@@ -1,4 +1,5 @@
 const City = require('../src/Models/City')
+const Grade = require('../src/Models/Grade')
 
 test('Can create a city object', () => {
     let city = new City();
@@ -7,4 +8,12 @@ test('Can create a city object', () => {
 test('Can create a city object by factory', () => {
     let city = City.create();
     expect(city).toBeInstanceOf(City);
+});
+test('Can create a grades object', () => {
+    let grade = new Grade();
+    expect(grade).toBeInstanceOf(Grade);
+});
+test('Can create a city object by factory', () => {
+    let grade = Grade.create();
+    expect(grade).toBeInstanceOf(Grade);
 });
