a262ea49833cd247f25353c400a55d46756131b2
Michelle Nunez <mpnunez5@gmail.com>
2021-02-26 23:43:30 -0500

    Created a Grade model

---
+++ src/Models/Grade.js
@@ -0,0 +1,20 @@
+module.exports = class Grade {
+    constructor(data = null) {
+        if(data) {
+            this.firstName = data.firstName;
+            this.lastName = data.lastName;
+            this.studentID = data.studentID;
+            this.homework1 = data.homework1;
+            this.homework2 = data.homework2;
+            this.homework3 = data.homework3;
+            this.homework4 = data.homework4;
+            this.homework5 = data.homework5;
+            this.total = data.total;
+        }
+
+    }
+    //Factory Method to Create a City
+    static create(data) {
+        return new Grades(data);
+    }
+}
